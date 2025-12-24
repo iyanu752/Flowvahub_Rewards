c986f3699d7745556ed1ded4538c5ce7e06fc022
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2025-12-24 21:10:03 +0100

    revert

--- flowvahub-assesment/src/components/auth/GoogleAuthButton.tsx
+++ flowvahub-assesment/src/components/auth/GoogleAuthButton.tsx
@@ -21,7 +21,7 @@ export const GoogleAuthButton: React.FC<GoogleAuthButtonProps> = ({
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: "https://flowvahub-rewards.vercel.app/auth/callback",
+          redirectTo: "https://gzqvmvnfuowmhseguymx.supabase.co/auth/v1/callback",
         },
       });
 
